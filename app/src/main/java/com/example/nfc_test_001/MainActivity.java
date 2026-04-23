8d8634073595a7eb5c84310dd0c94f2c749bf3d2
ryuseibs <beryusei0923212ssyo@aol.com>
2026-04-23 11:09:05 +0900

    実装：NFCの規格のログを出力

--- app/src/main/java/com/example/nfc_test_001/MainActivity.java
+++ app/src/main/java/com/example/nfc_test_001/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
     private NfcAdapter nfcAdapter;
@@ -63,6 +64,10 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
 
         // ログ出力
         Log.d("NFC_CHECK", "iPhone detected! ID:" + idString);
+
+        //NFC規格
+        String[] techList = tag.getTechList();
+        Log.d("NFC_CHECK", "Tech List:" + Arrays.toString(techList));
 
         // 音を鳴らす
         if(mediaPlayer != null) {
